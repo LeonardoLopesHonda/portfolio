f1e7e6df2e66704196d8c9c5e5ea707c4d92e101
leonardo honda <leozinhohondams@gmail.com>
2025-12-29 23:03:45 -0400

    Reapply "fix: make components inherit width"
    This reverts commit 0da4cf4ab0f4f018e27e0a5084ff5b570a8a8b94.

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,13 +1,13 @@
-import type Skill from "../interfaces/skills";
+import type skill from "../interfaces/skills";
 
 interface ChildProps {
-    skills: Skill[];
+    skills: skill[];
 }
 
 export default function Skills({ skills }: ChildProps) {
-    return <div className="w-3/5 mt-6 text-white flex flex-col items-start gap-4">
+    return <div className="w-full my-6 mb-4 text-white flex flex-col items-start gap-4">
         <p className="text-lg text-primary">My skills: </p>
-        <ul className="w-11/12 m-auto flex items-center justify-center flex-wrap gap-4">
+        <ul className="w-full flex items-center justify-start flex-wrap gap-4 gap-y-6">
            {skills.map(skill =>
             <li className="flex items-center justify-center gap-2 hover:-translate-y-3 hover:scale-110 transition-all duration-500 ease-out"><img className="w-8" src={skill.image} alt={skill.name} /> {skill.name}</li>
            )} 
